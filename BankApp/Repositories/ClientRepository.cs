62e2d359d69d821ecd44190c5b51b8241a0086aa
denkaty <denisselim2003@gmail.com>
2024-05-28 13:52:18 +0300

    add repositories

---
+++ BankApp/Repositories/ClientRepository.cs
@@ -0,0 +1,58 @@
+﻿using BankApp.Entities;
+using BankApp.Repositories.Contracts;
+
+namespace BankApp.Repositories
+{
+    public class ClientRepository : IClientRepository
+    {
+        //private static readonly object _lock = new object();
+        //private static ClientRepository _instance = null;
+        //private static Dictionary<int, Client> _clients;
+
+        //private ClientRepository() { }
+
+        //public static ClientRepository GetInstance
+        //{
+        //    get
+        //    {
+        //        lock (_lock)
+        //        {
+        //            if (_instance == null)
+        //            {
+        //                _instance = new ClientRepository();
+        //                _clients = new Dictionary<int, Client>();
+        //            }
+        
+        //            return _instance;
+        
+        //        }
+        //    }
+        //}
+
+        private readonly Dictionary<int, Client> _clients;
+
+        public ClientRepository()
+        {
+            _clients = new Dictionary<int, Client>();
+        }
+
+        public Client? GetById(int id)
+        {
+            if (!_clients.ContainsKey(id)) { return null; }
+
+            Client client = _clients[id];
+            return client;
+        }
+
+        public ICollection<Client> GetAll()
+        {
+            return _clients.Values;
+        }
+
+        public void Add(Client client)
+        {
+            _clients.Add(client.Id, client);
+        }
+
+    }
+}
