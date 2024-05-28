ec1c1a0eb1b41b2117a8fbc5f728d09c5ea18b27
denkaty <denisselim2003@gmail.com>
2024-05-28 13:49:29 +0300

    configuration

--- BankApp/Program.cs
+++ BankApp/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace BankApp
+﻿using BankApp.Configurators;
+using BankApp.Engines.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+namespace BankApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Run();
         }
+
+        private static void Run()
+        {
+            IHost host = Host
+               .CreateDefaultBuilder()
+               .ConfigureServices(ServiceConfigurator.Configure)
+               .Build();
+
+            IServiceProvider serviceProvider = host
+                .Services
+                .CreateScope()
+                .ServiceProvider;
+
+            IBankEngine bankEngine = serviceProvider
+                .GetRequiredService<IBankEngine>();
+
+            bankEngine.Run();
+        }
+
     }
 }
